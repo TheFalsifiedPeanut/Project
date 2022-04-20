5e289a2058ab876d02f0cb3cdbea9165cc22d74b
TheFalsifiedPeanut <81738969+TheFalsifiedPeanut@users.noreply.github.com>
2022-04-20 18:02:12 +1000

    Push

--- Project collect the dudes/Screens/Menu/MainMenu.cs
+++ Project collect the dudes/Screens/Menu/MainMenu.cs
@@ -6,29 +6,30 @@ namespace Collect_Dudes.Screens.Menu
     using System.Collections.Generic;
     using System.Text;
     using Utilities;
-    using ConsoleUtilities;
-    
+    using ConsoleUtilities.Inputs;
+    using ConsoleUtilities.Colour;
+
     class MainMenu : Screen
     {
         public override void Render()
         {
-            Colours.ColourLine(Colours.BlueFG + "Play");
-            Console.WriteLine(" 1. Play \n 2. Exit \n 3. Delete");
-            int result = TextUtils.GetChoice("Pick your favourite letter!", 3);
-            switch (result)
+            byte choice = Inputs.ChoiceDialogue("CHOOSE", "-Ominous-", new Dictionary<ConsoleKey, ChoiceEntry>()
+            {
+                { ConsoleKey.Enter , new ChoiceEntry("[","]","Play")},
+                { ConsoleKey.Escape , new ChoiceEntry("[","]","Exit")}
+               }, true);
+            switch (choice)
             {
                 case 0:
-                    Console.WriteLine("A was chosen.");
+                    ScreenManager.GetScreenManager().EnqueueScreen(new PlayMenu());
+                    ScreenManager.GetScreenManager().RenderNextScreen();
                     break;
                 case 1:
-                    Console.WriteLine("B was chosen.");
-                    break;
-                case 2:
-                    Console.WriteLine("C was chosen.");
-                    break;
-                default:
+                    Environment.Exit(0);
                     break;
             }
+            Console.WriteLine(choice);
+            
         }
     }
 }
---
+++ Project collect the dudes/Screens/Menu/PlayMenu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleUtilities.Inputs;
+
+namespace Collect_Dudes.Screens.Menu
+{
+    class PlayMenu : Screen
+    {
+        public override void Render()
+        {
+            byte choice = Inputs.ChoiceDialogue("CHOOSE", "-Ominous-", new Dictionary<ConsoleKey, ChoiceEntry>()
+            {
+                {ConsoleKey.UpArrow, new ChoiceEntry("[","]","New Game") },
+                {ConsoleKey.DownArrow, new ChoiceEntry("[","]","Load Game") },
+                {ConsoleKey.LeftArrow, new ChoiceEntry("[","]","Back") }
+            }, true);
+            switch (choice)
+            {
+                case 0:
+                    Console.WriteLine("Loading New Game...");
+                    break;
+                case 1:
+                    Console.WriteLine("Loading Saved Game...");
+                    break;
+                case 2:
+                    ScreenManager.GetScreenManager().EnqueueScreen(new MainMenu());
+                    ScreenManager.GetScreenManager().RenderNextScreen();
+                    break;
+            }
+        }
+    }
+}
