b51b306714b4608a40adc8ee84d2d615e605a3c5
TheFalsifiedPeanut <81738969+TheFalsifiedPeanut@users.noreply.github.com>
2022-04-21 18:05:49 +1000

    Comments

---
+++ Project collect the dudes/Data/Player/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collect_Dudes.Data.Player
+{
+    class Player
+    {
+        public string name;
+        public int day;
+        public int month;
+        public int year;
+
+        public Player(string name, int day, int month, int year)
+        {
+            this.name = name;
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        }
+
+    }
+}
--- Project collect the dudes/Program.cs
+++ Project collect the dudes/Program.cs
@@ -4,6 +4,8 @@
     using Screens.Menu;
     using System;
     using ConsoleUtilities;
+    using Serialization;
+    using Data.Player;
 
     class Program
     {
@@ -12,6 +14,18 @@
             new ScreenManager();
             ScreenManager.GetScreenManager().EnqueueScreen(new MainMenu());
             ScreenManager.GetScreenManager().RenderNextScreen();
+            /*Player[] players = new Player[3];
+            players[0] = new Player("Namer", 32, 13, 2023);
+            players[1] = new Player("Named", 0, 0, 0000);
+            players[2] = new Player("Naming", 3245, 53, 23);
+            Save<Player[]>.SaveData(players, @"PlayerSave.json");
+            Player[] playerData = Save<Player[]>.LoadData(@"PlayerSave.json");
+            for (int i = 0; i < playerData.Length; i++)
+            {
+                Player player = playerData[i];
+                Console.WriteLine(player.name + "\n" + player.day + "\n" + player.month + "\n" + player.year);
+            }*/
+            
         }
     }
 }
